be0770219084010881e2fe887456fd1668fd03b3
lizhemin15 <lizhemin15@126.com>
2026-05-06 05:48:01 +0800

    debug: 添加 embed ReadDir debug 日志

--- server.go
+++ server.go
@@ -15957,10 +15957,12 @@ func handleGovernanceExamplesList(w http.ResponseWriter, r *http.Request) {
 	// 从 embed.FS 读取示例文件列表
 	entries, err := governanceExamplesFS.ReadDir("examples/governance")
 	if err != nil {
+		log.Printf("[DEBUG] ReadDir error: %v", err)
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]interface{}{"success": false, "message": "读取示例目录失败"})
 		return
 	}
+	log.Printf("[DEBUG] ReadDir returned %d entries", len(entries))
 
 	type ExampleFile struct {
 		Name string `json:"name"`
