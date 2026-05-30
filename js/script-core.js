401b7b8d8a1b4c753f3115c5c853d0d905c1443c
lizhemin15 <lizhemin15@126.com>
2026-05-30 08:39:16 +0800

    fix: 使用parseEmoji函数确保本地SVG emoji渲染

--- js/script-core.js
+++ js/script-core.js
@@ -369,7 +369,7 @@ function showSessionWelcome() {
     // 新会话/空会话，显示快捷提示气泡
     if (typeof showQuickPrompts === 'function') showQuickPrompts();
     // 重新解析 emoji，确保动态插入的欢迎内容也使用 twemoji SVG
-    if (typeof twemoji !== 'undefined') twemoji.parse(messagesEl);
+    if (typeof parseEmoji === 'function') parseEmoji(messagesEl);
 }
 
 function fillPrompt(text) {
